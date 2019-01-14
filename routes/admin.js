cd7268e42e59082fa06b9738b28a686d69ea78cc
donovanCarreno <donovan_carreno@yahoo.com>
2019-01-13 19:13:39 -0600

    add handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,17 @@ const products = []
 
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+  // pug
+  // res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
+
+  // hbs
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  })
 })
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,18 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   const products = adminData.products
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'})
+  // pug
+  // res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'})
+
+  // hbs
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  })
 })
 
 module.exports = router
