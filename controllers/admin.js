ca8fe6976a8ae9a36ca1f6a4c053237bcf5bcdf1
donovanCarreno <donovan_carreno@yahoo.com>
2019-01-24 11:51:26 -0600

    dynamic routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,20 +6,51 @@ exports.getAddProduct = (req, res, next) => {
   // res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
 
   // hbs
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false
   })
 }
 
 exports.postAddProduct = (req, res, next) => {
   const {title, imageUrl, price, description} = req.body
-  const product = new Product(title, imageUrl, price, description)
+  const product = new Product(null, title, imageUrl, description, price)
   product.save()
   res.redirect('/')
+}
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit
+
+  if (!editMode) {
+    return res.redirect('/')
+  }
+
+  const prodId = req.params.productId
+
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/')
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product
+    })
+  })
+}
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedImageUrl = req.body.imageUrl
+  const updatedDescription = req.body.description
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice)
+  updatedProduct.save()
+  res.redirect('/admin/products')
 }
 
 exports.getProducts = (req, res, next) => {
@@ -30,4 +61,10 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     })
   })
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.deleteById(prodId)
+  res.redirect('/admin/products')
 }
